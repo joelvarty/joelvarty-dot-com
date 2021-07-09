ee024c91bb8b5c75c4c6bc4583dced78f2390618
Joel Varty <joel@agilitycms.com>
2021-07-09 15:18:26 -0400

    analytics

--- components/common/Layout.js
+++ components/common/Layout.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react'
+
+import { initGA, logPageView } from "../../lib/analytics"
+
 import { getPageTemplate } from "components/agility-pageTemplates";
 import { handlePreview } from "@agility/nextjs";
 import { useRouter } from "next/router";
@@ -12,62 +16,86 @@ import LoadingWidget from "./LoadingWidget";
 const handlingPreview = handlePreview();
 
 function Layout(props) {
-  const {
-    page,
-    sitemapNode,
-    dynamicPageItem,
-    notFound,
-    pageTemplateName,
-	isPreview,
-	isDevelopment
-  } = props;
+	const {
+		page,
+		sitemapNode,
+		dynamicPageItem,
+		notFound,
+		pageTemplateName,
+		isPreview,
+		isDevelopment
+	} = props;
 
 
-  // If the page is not yet generated, this will be displayed
-  // initially until getStaticProps() finishes running
-  const router = useRouter();
-  if (router.isFallback) {
-    return <LoadingWidget message="Loading Page" />;
-  }
+	const [isGaLoaded, setIsGaLoaded] = useState(false)
 
-  // if page not found, throw 404
-  if (notFound === true) {
-    return <Error statusCode={404} />;
-  }
+	// If the page is not yet generated, this will be displayed
+	// initially until getStaticProps() finishes running
+	const router = useRouter();
+	if (router.isFallback) {
+		return <LoadingWidget message="Loading Page" />;
+	}
 
-  const AgilityPageTemplate = getPageTemplate(pageTemplateName);
+	// if page not found, throw 404
+	if (notFound === true) {
+		return <Error statusCode={404} />;
+	}
 
-  if (dynamicPageItem?.seo?.metaDescription) {
-    page.seo.metaDescription = dynamicPageItem.seo.metaDescription;
-  }
+	const AgilityPageTemplate = getPageTemplate(pageTemplateName);
 
-  return (
-    <>
-      <SEO
-        title={sitemapNode?.title}
-        description={page.seo.metaDescription}
-        keywords={page.seo.metaKeywords}
-        metaHTML={page.seo.metaHTML}
-      />
-      <div id="site-wrapper">
-        {handlingPreview && <LoadingWidget message="Loading Preview Mode" />}
-        {!handlingPreview && (
-          <div id="site">
-			{ (isDevelopment || isPreview) &&
-            <PreviewBar {...props} />
-			}
-            <div className="flex flex-col min-h-screen">
-              <SiteHeader {...props} />
-              <main className="flex-grow">
-                <AgilityPageTemplate {...props} />
-              </main>
+	if (dynamicPageItem?.seo?.metaDescription) {
+		page.seo.metaDescription = dynamicPageItem.seo.metaDescription;
+	}
 
-            </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+	useEffect(() => {
+		if (typeof (window) === undefined) return
+
+		const handleRouteChange = (url) => {
+			logPageView(url)
+		};
+
+		if (!window.GA_INITIALIZED) {
+			initGA()
+			handleRouteChange(location.pathname)
+			window.GA_INITIALIZED = true
+		}
+
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		return () => {
+			if (typeof (window) === undefined) return
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+
+	}, [router.events]);
+
+	return (
+		<>
+			<SEO
+				title={sitemapNode?.title}
+				description={page.seo.metaDescription}
+				keywords={page.seo.metaKeywords}
+				metaHTML={page.seo.metaHTML}
+			/>
+			<div id="site-wrapper">
+				{handlingPreview && <LoadingWidget message="Loading Preview Mode" />}
+				{!handlingPreview && (
+					<div id="site">
+						{(isDevelopment || isPreview) &&
+							<PreviewBar {...props} />
+						}
+						<div className="flex flex-col min-h-screen">
+							<SiteHeader {...props} />
+							<main className="flex-grow">
+								<AgilityPageTemplate {...props} />
+							</main>
+
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
 
 export default Layout;
